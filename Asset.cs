a19690b2fda891f988bd1c58cd87f9000e3bac0f
OmarAlsharad <31482651+OmarAlsharad@users.noreply.github.com>
2018-11-29 19:30:11 -0800

    Add files via upload
    Updated versions of asset and inventory class

---
+++ Asset.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace COMPE361Project
+{
+    /// <summary>
+    /// Class for assets 
+    /// </summary>
+    public class Asset : IComparable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string IDnumber { get; set; }
+        public double Price { get; set; }
+        public int ModelNumber { get; set; }
+        public int SerialNumber { get; set; }
+        public bool CheckIn { get; set; }       
+
+        /// <summary>
+        /// Default constructor that initializes a new instance of the class.
+        /// </summary>
+        public Asset()
+        {
+            this.Name = "Empty";
+            this.Description = "Empty";
+            this.IDnumber = (Guid.NewGuid().ToString());
+            this.Price = 0;
+            this.ModelNumber = 0;
+            this.SerialNumber = 0;
+            this.CheckIn = false;
+        }
+
+        /// <summary>
+        /// Explicit value constructor that Initializes a new instance of the asset class.
+        /// </summary>
+        /// <param name="Name">Name.</param>
+        /// <param name="Description">Description.</param>
+        /// <param name="ModelNumber">Model number.</param>
+        /// <param name="SerialNumber">Serial number.</param>
+        /// <param name="CheckIn">If set to <c>true</c> check in.</param>
+        public Asset(string Name, string Description, double Price, int ModelNumber, int SerialNumber, bool CheckIn)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.IDnumber = Guid.NewGuid().ToString(); 
+            this.Price = Price;
+            this.ModelNumber = ModelNumber;
+            this.SerialNumber = SerialNumber;
+            this.CheckIn = CheckIn;
+        }
+
+        /// <summary>
+        /// Compares two assets based on their ID number
+        /// </summary>
+        /// <returns>Returns 1 if the current assets ID number is greater than the ID number of the asset
+        /// being compared to, -1 if less than, and 0 if equal to</returns>
+        /// <param name="obj">Object.</param>
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Asset) || obj == null)
+                throw new ArgumentException("The object passed is invalid");
+            Asset that = (Asset)obj;                //Cast the object as an Asset
+            //Compare the two ID numbers of the two assets
+            return string.Compare(this.IDnumber, that.IDnumber, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for an asset object.
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures such as a
+        /// hash table.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current asset.
+        /// </summary>
+        /// <returns>A string that represents the current asset.</returns>
+        public override string ToString()
+        {
+            return $"name: {this.Name}\nDescription: {this.Description}\nID number: {this.IDnumber}\nPrice: {this.Price}\n" +
+                $"Model Number: {this.ModelNumber}\nSerial Number: {this.SerialNumber}\nCheck In: {this.CheckIn}\n";
+        }
+        
+    }
+}
---
+++ Inventory.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace COMPE361Project
+{
+    /// <summary>
+    /// Class for the inventory
+    /// </summary>
+    public class Inventory
+    {
+        private List<Asset> listOfAssets = new List<Asset>();
+        private int NumberOfAssets { get; set; }
+
+        /// <summary>
+        /// Default constructor for the inventory class
+        /// </summary>
+        public Inventory()
+        {
+            NumberOfAssets = 0;
+        }
+
+        /// <summary>
+        /// Sorts the inventory based on the unique ID number for each asset
+        /// </summary>
+        public void SortInventory()
+        {
+            listOfAssets.Sort();
+        }
+
+        /// <summary>
+        /// Adds an asset to the inventory
+        /// </summary>
+        /// <param name="Name">Name.</param>
+        /// <param name="Description">Description.</param>
+        /// <param name="ModelNumber">Model number.</param>
+        /// <param name="SerialNumber">Serial number.</param>
+        /// <param name="CheckIn">If set to <c>true</c> check in.</param>
+        public void AddAsset(string Name = "None", string Description = "None", double Price = 0, int ModelNumber = 0, int SerialNumber = 0, bool CheckIn = false){
+            listOfAssets.Add(new Asset(Name, Description, Price, ModelNumber, SerialNumber, CheckIn));
+            listOfAssets.Sort();
+            NumberOfAssets++;
+        }
+
+        /// <summary>
+        /// Removes an asset from the inventory
+        /// </summary>
+        /// <param name="index">Index of the asset to be removed</param>
+        public void RemoveAsset(int index){
+            listOfAssets.RemoveAt(index);
+            NumberOfAssets--;
+        }
+
+        /// <summary>
+        /// Clears the inventory.
+        /// </summary>
+        public void ClearInventory(){
+            listOfAssets.Clear();
+            NumberOfAssets = 0;
+        }
+
+        /// <summary>
+        /// Finds the total value for all assets in the inventory
+        /// </summary>
+        /// <returns>The total value for the inventory</returns>
+        public double FindTotalValue(){
+            double TotalValue = 0;
+            foreach (Asset A in listOfAssets){
+                TotalValue += A.Price;
+            }
+            return TotalValue;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current inventory
+        /// </summary>
+        /// <returns>A string that represents the current inventory.</returns>
+        public override string ToString()
+        {
+            string s = "";
+            foreach (Asset A in listOfAssets)
+            {
+                s += A.ToString();
+                s += "****************************************\n";
+            }
+            return s;
+        }
+    }
+
+    /// <summary>
+    /// Main class
+    /// </summary>
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Inventory x = new Inventory();
+            x.AddAsset("Omar's phone", "Good phone",700, 28, 222222, true);
+            x.AddAsset("David's phone", "Bad phone (it's not an iphone)", 300, 12, 124, true);
+            x.AddAsset("Amack's phone", "Probably really good", 7000.1, 444, 1234, false);
+            Console.WriteLine(x);
+
+        }
+    }
+}
